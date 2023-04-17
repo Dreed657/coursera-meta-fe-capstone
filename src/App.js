9edfe6396f5ebd453587b4899f94d8c9498159d6
Dreed657 <ninjakiviWTF@gmail.com>
2023-04-17 22:59:08 +0300

    add booking form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import { Box, Typography } from '@mui/joy';
+import BookingForm from './bookingForm/BookingForm';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className="App">
+      <Typography variant="h1" fontSize={15}>
+        Reserve a table
+      </Typography>
+      <BookingForm />
+    </Box>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders reserve a table form', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Reserve a table/i);
   expect(linkElement).toBeInTheDocument();
 });
